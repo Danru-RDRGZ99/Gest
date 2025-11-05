a7952dbf1ff547e8ec2de6d0ff164d660898f5b6
Danru-RDRGZ99 <144158038+Danru-RDRGZ99@users.noreply.github.com>
2025-11-04 21:40:06 -0600

    Update docker-entrypoint.sh

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,13 +1,5 @@
-#!/bin/sh
-
-# Configura el script para fallar si hay un error
 set -eu
 
-# 1. Lee la plantilla (nginx.conf.template)
-# 2. Reemplaza las variables ($USUARIOS_HOSTPORT, etc.) con los valores
-#    que Render inyectó en el entorno.
-# 3. Guarda el resultado en el archivo de configuración real de Nginx.
 envsubst '$USUARIOS_HOSTPORT,$RESERVAS_HOSTPORT,$INVENTARIO_HOSTPORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# 4. Inicia el servidor Nginx en primer plano.
 nginx -g 'daemon off;'
