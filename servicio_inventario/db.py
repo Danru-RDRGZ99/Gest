fa5e4308fab7c26240fc8ad989f70826db73e9ba
Danru-RDRGZ99 <a22985@universidad-une.com>
2025-11-05 11:43:06 -0600

    `Refactor database connection and security settings in db.py and security_inventario.py`

--- servicio_inventario/db.py
+++ servicio_inventario/db.py
@@ -1,12 +1,23 @@
-import os
+# db.py (Inventario)
+from starlette.config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+config = Config(".env")
+DATABASE_URL = config("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=5,         # puedes subir a 8-10 si tu Postgres lo permite
+    max_overflow=5,      # controla picos, evita colas largas
+    pool_pre_ping=True,  # evita conexiones muertas
+    pool_recycle=300,    # recicla antes de que el proxy cierre
+    future=True,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, expire_on_commit=False, bind=engine
+)
 
 def get_db():
     db = SessionLocal()
